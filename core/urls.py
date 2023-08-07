0524632a719bd53b5a762cdb5a1dc38cfa510818
Nícolas Grande <nicolas.grande@inteliger.com.br>
2023-08-07 00:29:37 -0300

    projeto

--- core/urls.py
+++ core/urls.py
@@ -7,7 +7,7 @@ import core.views
 
 urlpatterns = [
                   re_path(r'^presentes$', core.views.ProdutosView.as_view(), name="produtos"),
-                  re_path(r'^confirmar$', core.views.ProdutosView.as_view(), name="produtos"),
+                  re_path(r'^confirmar$', core.views.ProdutosView.as_view(), name="confirmar"),
                   re_path(r'^$', core.views.HomeView.as_view(), name="home"),
               ] \
               + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) \
