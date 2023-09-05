bc08e8805e17ae651a66274817d41bc9fae5122d
Nícolas Marinoni Grande <nicolas.grande@inteliger.com.br>
2023-09-05 01:29:29 -0300

    confir

--- casamentoPeL/settings.py
+++ casamentoPeL/settings.py
@@ -138,7 +138,7 @@ STATICFILES_FINDERS = [
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
 ]
-# commit
+# commi
 IS_DEV = False
 if IS_DEV:
     STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
